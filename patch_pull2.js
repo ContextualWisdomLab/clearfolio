b28574e9ca53f524a909c10c66b47c5508a4c22e
Seongho Bae <me@seonghobae.me>
2026-08-17 12:33:00 +0900

    chore(scope): remove temporary branch-repair script

--- patch_pull2.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const { execSync } = require('child_process');
-
-try {
-  execSync('git fetch origin main');
-  execSync('git reset --hard origin/main');
-} catch (e) {
-  console.log(e.toString());
-}

2c077077ef426828d856838125b35e7cc63f3241
Seongho Bae <me@seonghobae.me>
2026-09-05 01:43:11 +0900

    test(auth): reject NUL-bearing signing secret configuration

--- src/test/java/com/clearfolio/viewer/auth/TenantAccessServiceTest.java
+++ src/test/java/com/clearfolio/viewer/auth/TenantAccessServiceTest.java
@@ -44,14 +44,16 @@ class TenantAccessServiceTest {
     }
 
     @Test
-    void requireSkipsSignatureValidationWhenSecretIsBlankOrNull() {
+    void requireSkipsSignatureValidationOnlyWhenSecretIsAbsentOrBlank() {
         TenantAccessService blankSecret = new TenantAccessService(" ", 300L, Clock.fixed(NOW, ZoneOffset.UTC));
         TenantAccessService nullSecret = new TenantAccessService(null, 300L, Clock.fixed(NOW, ZoneOffset.UTC));
-        TenantAccessService nullCharSecret = new TenantAccessService("\u0000", 300L, Clock.fixed(NOW, ZoneOffset.UTC));
 
         assertDoesNotThrow(() -> blankSecret.require(headers(TenantPermissions.JOB_READ), TenantPermissions.JOB_READ));
         assertDoesNotThrow(() -> nullSecret.require(headers(TenantPermissions.JOB_READ), TenantPermissions.JOB_READ));
-        assertDoesNotThrow(() -> nullCharSecret.require(headers(TenantPermissions.JOB_READ), TenantPermissions.JOB_READ));
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> new TenantAccessService("\u0000", 300L, Clock.fixed(NOW, ZoneOffset.UTC))
+        );
     }
 
     @Test

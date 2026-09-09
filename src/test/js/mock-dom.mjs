7a99a347013e0c74f6035d56012c5a30e2a37414
seonghobae <8172694+seonghobae@users.noreply.github.com>
2026-09-09 21:16:32 +0000

    ⚡ Bolt: [성능 개선] DocumentFragment를 사용한 DOM 렌더링 최적화

--- src/test/js/mock-dom.mjs
+++ src/test/js/mock-dom.mjs
@@ -5,6 +5,34 @@ export class MockTextNode {
   }
 }
 
+export class MockDocumentFragment {
+  constructor() {
+    this.nodeType = 11;
+    this.childNodes = [];
+  }
+
+  appendChild(node) {
+    if (node && node.nodeType === 11) {
+      this.childNodes.push(...node.childNodes);
+      node.childNodes = [];
+      return node;
+    }
+    this.childNodes.push(node);
+    return node;
+  }
+
+  append(...nodes) {
+    for (const node of nodes) {
+      if (node && node.nodeType === 11) {
+        this.childNodes.push(...node.childNodes);
+        node.childNodes = [];
+      } else {
+        this.childNodes.push(node);
+      }
+    }
+  }
+}
+
 export class MockElement {
   constructor(tagName = "div") {
     this.tagName = tagName.toUpperCase();
@@ -31,12 +59,24 @@ export class MockElement {
   }
 
   appendChild(node) {
+    if (node && node.nodeType === 11) {
+      this.childNodes.push(...node.childNodes);
+      node.childNodes = [];
+      return node;
+    }
     this.childNodes.push(node);
     return node;
   }
 
   append(...nodes) {
-    this.childNodes.push(...nodes);
+    for (const node of nodes) {
+      if (node && node.nodeType === 11) {
+        this.childNodes.push(...node.childNodes);
+        node.childNodes = [];
+      } else {
+        this.childNodes.push(node);
+      }
+    }
   }
 
   replaceChildren(...nodes) {

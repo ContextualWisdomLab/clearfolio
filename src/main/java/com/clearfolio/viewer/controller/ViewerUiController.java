1e2b7f38162cda5e11ff3a95398f0e48b6bc3ce7
seonghobae <8172694+seonghobae@users.noreply.github.com>
2026-09-09 14:26:13 +0000

    🎨 Palette: 폼 필드 필수 입력 시각적 표시 추가 (접근성 개선)

--- src/main/java/com/clearfolio/viewer/controller/ViewerUiController.java
+++ src/main/java/com/clearfolio/viewer/controller/ViewerUiController.java
@@ -186,7 +186,7 @@ public class ViewerUiController {
 
                         <form id="upload-form" class="upload-form" enctype="multipart/form-data">
                           <label class="field-label" for="file-input">
-                            <span>Document <span class="error__title"
+                            <span>Document <span class="required-indicator"
                                   aria-hidden="true">*</span></span>
                           </label>
                           <input id="file-input" name="file" class="file-input" type="file" required />

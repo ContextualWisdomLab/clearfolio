1ccab0bfa780c0fcd461f8ce0899955b7e47d272
Seongho Bae <me@seonghobae.me>
2026-09-11 01:07:14 +0900

    revert(perf): remove unmeasured history DOM batching

--- src/main/resources/static/assets/viewer/demo.js
+++ src/main/resources/static/assets/viewer/demo.js
@@ -114,8 +114,6 @@ function renderHistory(history = loadHistory()) {
   el.historyBody.textContent = "";
   el.emptyHistory.hidden = history.length > 0;
 
-  const fragment = document.createDocumentFragment();
-
   for (const job of history) {
     const row = document.createElement("tr");
     const fileCell = document.createElement("td");
@@ -145,10 +143,8 @@ function renderHistory(history = loadHistory()) {
     }
 
     row.append(fileCell, statusCell, submittedCell, actionsCell);
-    fragment.appendChild(row);
+    el.historyBody.appendChild(row);
   }
-
-  el.historyBody.appendChild(fragment);
 
   renderRecoveryEvidence(history);
 }
--- src/test/js/mock-dom.mjs
+++ src/test/js/mock-dom.mjs
@@ -5,34 +5,6 @@ export class MockTextNode {
   }
 }
 
-export class MockDocumentFragment {
-  constructor() {
-    this.nodeType = 11;
-    this.childNodes = [];
-  }
-
-  appendChild(node) {
-    if (node && node.nodeType === 11) {
-      this.childNodes.push(...node.childNodes);
-      node.childNodes = [];
-      return node;
-    }
-    this.childNodes.push(node);
-    return node;
-  }
-
-  append(...nodes) {
-    for (const node of nodes) {
-      if (node && node.nodeType === 11) {
-        this.childNodes.push(...node.childNodes);
-        node.childNodes = [];
-      } else {
-        this.childNodes.push(node);
-      }
-    }
-  }
-}
-
 export class MockElement {
   constructor(tagName = "div") {
     this.tagName = tagName.toUpperCase();
@@ -59,24 +31,12 @@ export class MockElement {
   }
 
   appendChild(node) {
-    if (node && node.nodeType === 11) {
-      this.childNodes.push(...node.childNodes);
-      node.childNodes = [];
-      return node;
-    }
     this.childNodes.push(node);
     return node;
   }
 
   append(...nodes) {
-    for (const node of nodes) {
-      if (node && node.nodeType === 11) {
-        this.childNodes.push(...node.childNodes);
-        node.childNodes = [];
-      } else {
-        this.childNodes.push(node);
-      }
-    }
+    this.childNodes.push(...nodes);
   }
 
   replaceChildren(...nodes) {
